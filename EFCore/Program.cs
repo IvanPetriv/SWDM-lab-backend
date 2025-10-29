4ae7e4074465605c2725d3e9e0be9268d09aaa28
ІВАН <ivan47044704@gmail.com>
2025-10-29 17:00:35 +0200

    Use faker in efcore project

--- EFCore/Program.cs
+++ EFCore/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EFCore;
+
+var teachers = FakeDataGenerator.GenerateTeachers();
+var courses = FakeDataGenerator.GenerateCourses(teachers);
+var students = FakeDataGenerator.GenerateStudents();
+var enrollments = FakeDataGenerator.GenerateEnrollments(students, courses);
+
+using UniversityDbContext context = new();
+context.Teachers.AddRange(teachers);
+context.Courses.AddRange(courses);
+context.Students.AddRange(students);
+context.Enrollments.AddRange(enrollments);
+context.SaveChanges();
